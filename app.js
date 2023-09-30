fe2e9ec9619cb7bb7654324c01b087e9b2559364
pac <sunpochin@gmail.com>
2023-10-01 00:55:25 +0800

    use 3.5

--- app.js
+++ app.js
@@ -2,15 +2,17 @@ require('dotenv').config();
 
 const line = require('@line/bot-sdk');
 const express = require('express');
-// const { Configuration, OpenAIApi } = require('openai');
-const { OpenAI } = require('openai');
 
-// const configuration = new Configuration({
-//   apiKey: process.env.OPENAI_API_KEY,
-// });
-const openai = new OpenAI({
+const { Configuration, OpenAIApi } = require('openai');
+const configuration = new Configuration({
   apiKey: process.env.OPENAI_API_KEY,
 });
+const openai = new OpenAIApi(configuration);
+
+// const { OpenAI } = require('openai');
+// const openai = new OpenAI({
+//   apiKey: process.env.OPENAI_API_KEY,
+// });
 
 // create LINE SDK config from env variables
 const config = {
@@ -51,16 +53,48 @@ async function handleEvent(event) {
     return Promise.resolve(null);
   }
 
-  const completion = await openai.createCompletion({
-    model: "text-davinci-003",
-    prompt: event.message.text ,
+  // const completion = await openai.createCompletion({
+  //   model: "text-davinci-003",
+  //   prompt: event.message.text,
+  //   max_tokens: 256,
+  //   // temperature: 0.7,
+  //   // top_p: 1,
+  //   // frequency_penalty: 0,
+  //   // presence_penalty: 0,
+  // });
+  const { data } = await openai.createChatCompletion({
+    model: "gpt-3.5-turbo",
+    messages: [{
+      role: 'user',
+      content: event.message.text,
+    }],
+    max_tokens: 200,
   });
 
-  // create a echoing text message
-  const echo = { type: 'text', text: completion.data.choices[0].text };
+  const [choices] = data.choices;
 
-  // use reply API
+  const echo = { type: 'text', text: choices.message.content.trim() || '抱歉，我沒有話可說了。' };
+
   return client.replyMessage(event.replyToken, echo);
+
+  // async function ask(prompt) {
+  //     const response = await openai.createCompletion({
+  //         model: "text-davinci-002",
+  //         prompt,
+  //         temperature: 0.7,
+  //         max_tokens: 256,
+  //         top_p: 1,
+  //         frequency_penalty: 0,
+  //         presence_penalty: 0,
+  //     });
+  //   }
+  // ask(event.message.text);
+
+  // // create a echoing text message
+  // const echo = { type: 'text', text: completion.data.choices[0].text };
+
+  // // use reply API
+  // return client.replyMessage(event.replyToken, echo);
 
   // // create a echoing text message
   // const echo = { type: 'text', text: event.message.text };
